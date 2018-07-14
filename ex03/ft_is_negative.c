8282ff717f3a522c5603a28d43efcc3004ad4a3b
Shanaya Jain <shanayajain2001@gmail.com>
2018-07-14 11:00:31 -0700

    excercise 3 negative or not

---
+++ ex03/ft_is_negative.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void ft_is_negative (int n) {
+    if (n < 0){
+        printf("N");
+        printf("\n");
+    }
+    else {
+        printf("P");
+        printf("\n");
+    }
+}
+
+int main() {
+   ft_is_negative(-7);
+
+}
